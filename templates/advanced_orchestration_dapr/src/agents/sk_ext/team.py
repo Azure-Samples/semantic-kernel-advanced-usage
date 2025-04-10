85cec208a1c6d12d815df9304bce8232fa88e433
Riccardo Chiodaroli <ricchi@microsoft.com>
2025-04-10 10:45:25 +0200

    update: Team

--- templates/advanced_orchestration_dapr/src/agents/sk_ext/team.py
+++ templates/advanced_orchestration_dapr/src/agents/sk_ext/team.py
@@ -1,14 +1,22 @@
 import logging
 
 from collections.abc import AsyncIterable
-from typing import Any, ClassVar
+from typing import Any, Awaitable, Callable, ClassVar
 import sys
 
 if sys.version_info >= (3, 12):
     from typing import override  # pragma: no cover
 else:
     from typing_extensions import override  # pragma: no cover
-from semantic_kernel.agents import Agent
+from pydantic import Field
+from semantic_kernel import Kernel
+from semantic_kernel.agents import (
+    Agent,
+    AgentThread,
+    AgentResponseItem,
+    ChatHistoryAgentThread,
+)
+from semantic_kernel.agents.group_chat.agent_chat_utils import KeyEncoder
 from semantic_kernel.agents.strategies.selection.selection_strategy import (
     SelectionStrategy,
 )
@@ -52,17 +60,23 @@ class Team(Agent):
     selection_strategy: SelectionStrategy
     termination_strategy: TerminationStrategy
     channel_type: ClassVar[type[AgentChannel]] = ChatHistoryChannel
+    agent_channels: dict[str, AgentChannel] = Field(default_factory=dict)
+    channel_map: dict[Agent, str] = Field(default_factory=dict)
     is_complete: bool = False
 
     @trace_agent_get_response
     @override
     async def get_response(
         self,
-        history: ChatHistory,
+        *,
+        messages: (
+            str | ChatMessageContent | list[str | ChatMessageContent] | None
+        ) = None,
+        thread: AgentThread | None = None,
         arguments: KernelArguments | None = None,
         kernel: "Kernel | None" = None,
         **kwargs: Any,
-    ) -> ChatMessageContent:
+    ) -> AgentResponseItem[ChatMessageContent]:
         """Get a response from the agent.
 
         Args:
@@ -74,24 +88,49 @@ class Team(Agent):
         Returns:
             A chat message content.
         """
+        thread = await self._ensure_thread_exists_with_messages(
+            messages=messages,
+            thread=thread,
+            construct_thread=lambda: ChatHistoryAgentThread(),
+            expected_type=ChatHistoryAgentThread,
+        )
+        assert thread.id is not None  # nosec
+
+        chat_history = ChatHistory()
+        async for message in thread.get_messages():
+            chat_history.add_message(message)
+
         responses: list[ChatMessageContent] = []
-        async for response in self._inner_invoke(history, arguments, kernel, **kwargs):
+        async for response in self._inner_invoke(
+            history=chat_history,
+            arguments=arguments,
+            kernel=kernel,
+            on_intermediate_message=None,
+            **kwargs,
+        ):
             responses.append(response)
 
         if not responses:
             raise AgentInvokeException("No response from agent.")
 
-        return responses[0]
+        return AgentResponseItem(message=responses[-1], thread=thread)
 
     @trace_agent_invocation
     @override
     async def invoke(
         self,
-        history: ChatHistory,
+        *,
+        messages: (
+            str | ChatMessageContent | list[str | ChatMessageContent] | None
+        ) = None,
+        thread: AgentThread | None = None,
+        on_intermediate_message: (
+            Callable[[ChatMessageContent], Awaitable[None]] | None
+        ) = None,
         arguments: KernelArguments | None = None,
         kernel: "Kernel | None" = None,
         **kwargs: Any,
-    ) -> AsyncIterable[ChatMessageContent]:
+    ) -> AsyncIterable[AgentResponseItem[ChatMessageContent]]:
         """Invoke the chat history handler.
 
         Args:
@@ -103,12 +142,35 @@ class Team(Agent):
         Returns:
             An async iterable of ChatMessageContent.
         """
-        async for response in self._inner_invoke(history, arguments, kernel, **kwargs):
-            yield response
+        thread = await self._ensure_thread_exists_with_messages(
+            messages=messages,
+            thread=thread,
+            construct_thread=lambda: ChatHistoryAgentThread(),
+            expected_type=ChatHistoryAgentThread,
+        )
+        assert thread.id is not None  # nosec
+
+        chat_history = ChatHistory()
+        async for message in thread.get_messages():
+            chat_history.add_message(message)
+
+        async for response in self._inner_invoke(
+            thread=thread,
+            history=chat_history,
+            arguments=arguments,
+            kernel=kernel,
+            on_intermediate_message=on_intermediate_message,
+            **kwargs,
+        ):
+            yield AgentResponseItem(message=response, thread=thread)
 
     async def _inner_invoke(
         self,
+        thread: ChatHistoryAgentThread,
         history: ChatHistory,
+        on_intermediate_message: (
+            Callable[[ChatMessageContent], Awaitable[None]] | None
+        ) = None,
         arguments: KernelArguments | None = None,
         kernel: "Kernel | None" = None,
         **kwargs: Any,
@@ -116,20 +178,19 @@ class Team(Agent):
         # In case the agent is invoked multiple times
         self.is_complete = False
 
-        # Channel required to communicate with agents
-        channel = await self.create_channel()
-        await channel.receive(history.messages)
-
         # TODO: check if it makes sense to have a termination strategy here
         for _ in range(self.termination_strategy.maximum_iterations):
             try:
                 selected_agent = await self.selection_strategy.next(
-                    self.agents, history.messages
+                    self.agents, history=history
                 )
             # TODO: possible handle a case when no agent is selected
             except Exception as ex:
                 logger.error(f"Failed to select agent: {ex}")
                 raise AgentChatException("Failed to select agent") from ex
+
+            # Channel required to communicate with agents
+            channel = await self._get_or_create_channel(selected_agent, history)
 
             async for is_visible, message in channel.invoke(selected_agent):
                 history.add_message(message)
@@ -178,3 +239,26 @@ class Team(Agent):
 
             for message in messages:
                 history.messages.append(message)
+
+    def _get_agent_hash(self, agent: Agent):
+        """Get the hash of an agent."""
+        hash_value = self.channel_map.get(agent, None)
+        if hash_value is None:
+            hash_value = KeyEncoder.generate_hash(agent.get_channel_keys())
+            self.channel_map[agent] = hash_value
+
+        return hash_value
+
+    async def _get_or_create_channel(
+        self, agent: Agent, history: ChatHistory
+    ) -> AgentChannel:
+        """Get or create a channel."""
+        channel_key = self._get_agent_hash(agent)
+        channel = self.agent_channels.get(channel_key, None)
+        if channel is None:
+            channel = await agent.create_channel()
+            self.agent_channels[channel_key] = channel
+
+            if len(history.messages) > 0:
+                await channel.receive(history.messages)
+        return channel
